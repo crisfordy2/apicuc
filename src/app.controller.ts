54edaae8ecd0bd080230fce262e8532eed497f86
Cristian Gomez <gomez18cristian@gmail.com>
2021-09-04 15:27:35 -0500

    add users

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get(':id/code/:user_code')
+  getHello(@Param('id') userId:number, @Param('user_code') code:String  ): string {    
+    console.log("el id: ", userId);
+    console.log("code: ", code);
     return this.appService.getHello();
+
+  }
+
+  @Post(':id')
+  postData(@Param('id') userId:number, @Body() data:any):String {
+    console.log("data", data)
+    return `metodo post good ${userId}`;
   }
 }
+
